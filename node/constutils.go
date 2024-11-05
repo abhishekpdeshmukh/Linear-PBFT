7fc4a4ffedf16a27a3bf65f592f9604c7827a393
abhishekpdeshmukh <38159363+abhishekpdeshmukh@users.noreply.github.com>
2024-11-04 22:05:12 -0500

    Node constants

---
+++ node/constutils.go
@@ -0,0 +1,14 @@
+package main
+
+const (
+	StatusPrePrepared = "PP" // Pre-prepared
+	StatusPrepared    = "P"  // Prepared
+	StatusCommitted   = "C"  // Committed
+	StatusExecuted    = "E"  // Executed
+	StatusNoStatus    = "X"  // No Status
+)
+
+const (
+	F = 2
+	N = 7
+)
