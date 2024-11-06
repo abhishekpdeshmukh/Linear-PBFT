13ff89545eccb9cf4254911360d14b5ee7a09817
abhishekpdeshmukh <38159363+abhishekpdeshmukh@users.noreply.github.com>
2024-11-06 18:40:55 -0500

    Added a few TSS helper functions

--- client/netutils.go
+++ client/netutils.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"crypto/ed25519"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"log"
@@ -11,6 +13,10 @@ import (
 	"time"
 
 	pb "github.com/abhishekpdeshmukh/LINEAR-PBFT/proto"
+	"go.dedis.ch/kyber/v3"
+	"go.dedis.ch/kyber/v3/pairing/bn256"
+	"go.dedis.ch/kyber/v3/share"
+	"go.dedis.ch/kyber/v3/util/random"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
@@ -68,4 +74,74 @@ func broadcastTransaction(tx *pb.TransactionRequest) {
 			}
 		}(i)
 	}
+}
+
+func generateMasterKeyPairAndShares() (kyber.Scalar, kyber.Point, []*share.PriShare, *share.PubPoly) {
+	suite := bn256.NewSuite()
+	// Generate a master private and public key
+	masterPrivateKey := suite.G2().Scalar().Pick(random.New())
+	masterPublicKey := suite.G2().Point().Mul(masterPrivateKey, nil)
+
+	fmt.Println("Master Public Key:", masterPublicKey)
+
+	// Create a private polynomial for secret sharing
+	priPoly := share.NewPriPoly(suite.G2(), 2*F+1, masterPrivateKey, random.New())
+	priShares := priPoly.Shares(N)
+
+	// Create a public polynomial for verification
+	pubPoly := priPoly.Commit(suite.G2().Point().Base())
+
+	return masterPrivateKey, masterPublicKey, priShares, pubPoly
+}
+
+func initiateTSSHandshake(masterPublicKey kyber.Point, priShares []*share.PriShare, pubPoly *share.PubPoly) {
+
+	for i := 1; i <= 7; i++ {
+		go func(serverID int) {
+			var masterPublicKeyBytes []byte
+			var privateShareBytes []byte
+			var publicPolyCommitments []byte
+			masterPublicKeyBytes, _ = masterPublicKey.MarshalBinary()
+
+			privateShareBytes, err := serializePriShare(priShares[serverID])
+			if err != nil {
+				// Handle error
+			}
+
+			publicPolyCommitments, _ = pubPoly.Commit().MarshalBinary()
+
+			tssKeyExchange := &pb.TSSKeyExchange{
+				MasterPublicKey:       masterPublicKeyBytes,
+				PrivateShares:         privateShareBytes,
+				PublicPolyCommitments: publicPolyCommitments,
+			}
+			c, ctx, conn := setupClientSender(serverID)
+			defer conn.Close()
+			_, err = c.SendTSSKeys(ctx, tssKeyExchange)
+			if err != nil {
+				log.Printf("Failed to sendd TSS KEy exchange to server %d: %v", serverID, err)
+			}
+		}(i)
+	}
+}
+
+func serializePriShare(share *share.PriShare) ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	// Serialize the index (I)
+	err := binary.Write(buf, binary.BigEndian, int32(share.I))
+	if err != nil {
+		return nil, err
+	}
+
+	// Serialize the scalar value (V)
+	vBytes, err := share.V.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	// Write the scalar bytes to the buffer
+	buf.Write(vBytes)
+
+	return buf.Bytes(), nil
 }
